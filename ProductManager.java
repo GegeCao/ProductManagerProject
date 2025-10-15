589a16c6a9bf95913ff042e59f9c6b16efc3ad1f
GegeCao <caoge@sheridancollege.ca>
2025-10-15 14:23:40 -0400

    Added discount feature by Gege Cao

--- ProductManager.java
+++ ProductManager.java
@@ -2,4 +2,10 @@ public class ProductManager {
     public static void main(String[] args) {
         System.out.println("Product Manager initialized.");
     }
+
+    // Calculates discount based on percentage
+    public double calculateDiscount(double price, double percent) {
+        return price - (price * percent / 100);
+
+    }
 }
